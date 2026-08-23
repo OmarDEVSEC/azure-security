fb8ba04f604195d97b808fa0dbfa94f4cbe0c089
OmarDevSec <omarsecure30@gmail.com>
2026-08-22 22:38:55 -0500

    Variable calls created for monitoring module

---
+++ terraform/modules/monitoring/variables.tf
@@ -0,0 +1,30 @@
+
+
+// Monitoring variables
+
+variable "resource_group_name"{
+    description = "Resource group this modules resources belong to"
+    type        = string
+}
+
+variable "location"{
+    description = "Azure region for resources"
+    type        = string
+    default     = "Central US"
+}
+
+variable "alert_email"{
+    description = "Email address to recieve cost and service health alerts"
+    type        = string
+}
+
+variable "monthly_budget_amount"{
+    description = "Monthly budget threshold in USD"
+    type        = number
+    default     = 15
+}
+
+variable "subscription_id" {
+    description = "Azure subscription ID"
+    type        = strin
+}
