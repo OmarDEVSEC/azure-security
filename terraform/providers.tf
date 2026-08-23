b75b07abbcd0db441567b3c143571306bb0d4335
OmarDevSec <omarsecure30@gmail.com>
2026-08-22 21:19:26 -0500

    plugin that translates Terraform's generic language into actual Azure API calls. azurerm is HashiCorp's official Azure provider

---
+++ terraform/providers.tf
@@ -0,0 +1,14 @@
+# Terrform inital provider code to connect to Azure
+
+terraform {
+    required_providers{
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = "~> 3.0"
+        }
+    }
+}
+
+provider "azurerm" {
+    features {}
+}
