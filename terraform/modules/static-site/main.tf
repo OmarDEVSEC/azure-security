aecb2b14f568afe9583e5cf60793dc9c7bc3ce3f
OmarDevSec <omarsecure30@gmail.com>
2026-08-27 16:24:12 -0500

    Updated deprecated staticsit to static web app

--- terraform/modules/static-site/main.tf
+++ terraform/modules/static-site/main.tf
@@ -1,11 +1,15 @@
 //Create resource and set varibale to the resource for Azure static web app
 
-resource "azurerm_static_site" "main"{
+resource "azurerm_static_web_app" "main"{
     name                   =           var.site_name
     resource_group_name    =           var.resource_group_name
     location               =           var.location
     sku_tier               =           var.sku_tier
     sku_size               =           var.sku_tier
+
+    tags = {
+        PersonalSite = "1"
+    }
 
 }
 
