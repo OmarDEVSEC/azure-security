dcd0544d72063de57a6159fce407492d5ef37dfc
OmarDevSec <omarsecure30@gmail.com>
2026-09-05 23:56:24 -0500

    Unsure

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -47,7 +47,7 @@ resource "azurerm_public_ip" "main"{
 
 }
 
-resource "azurerm_network_inteface" "main"{
+resource "azurerm_network_interface" "main"{
     name                = "nic-azsec"
     location            = var.location
     resource_group_name = var.resource_group_name
@@ -55,7 +55,7 @@ resource "azurerm_network_inteface" "main"{
     ip_configuration {
         name                           = "internal"
         subnet_id                      = azurerm_subnet.main.id
-        private_ip_address_allocations = "Dynamic"
+        private_ip_address_allocation = "Dynamic"
         public_ip_address_id           = azurerm_public_ip.main.id
     }
 }
