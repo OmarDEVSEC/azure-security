c7ecb1bf8cfb64eb6e4920371b6665d107996c76
OmarDevSec <omarsecure30@gmail.com>
2026-09-05 22:40:19 -0500

    VNET,Subnet,NSG, NSG association, public IP -> NIC, and VM terraform resolves the build order automatically.

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -38,4 +38,59 @@ resource "azurerm_subnet_network_security_group_association" "main" {
   network_security_group_id = azurerm_network_security_group.main.id
 }
 
+resource "azurerm_public_ip" "main"{
+    name                = "pip-azsec"
+    location            = var.location
+    resource_group_name = var.resource_group_name
+    allocation_method   = "Static"
+    sku                 = "Standard"
 
+}
+
+resource "azurerm_network_inteface" "main"{
+    name                = "nic-azsec"
+    location            = var.location
+    resource_group_name = var.resource_group_name
+
+    ip_configuration {
+        name                           = "internal"
+        subnet_id                      = azurerm_subnet.main.id
+        private_ip_address_allocations = "Dynamic"
+        public_ip_address_id           = azurerm_public_ip.main.id
+    }
+}
+
+
+resource "azurerm_linux_virtual_machine" "main" {
+    name                = var.vm_name
+    resource_group_name = var.resource_group_name
+    location            = var.location
+    size                = var.vm_size
+    admin_username      = var.admin_username
+
+    network_interface_ids = [
+        azurerm_network_interface.main.id
+    ]
+
+    admin_ssh_key{
+        username        = var.admin_username
+        public_key      = var.ssh_public_key
+    }
+    disable_password_authentication = true
+
+    os_disk{
+        caching              = "ReadWrite"
+        storage_account_type = "Standard_LRS"
+    }
+
+    source_image_reference{
+        publisher            = "Canonical"
+        offer                = "0001-com-ubuntu-server-jammy"
+        sku                  = "22_04-lts-gen2"
+        version              = "latest"
+    }
+
+    tags = {
+        project = "azure-security"
+    }
+}
