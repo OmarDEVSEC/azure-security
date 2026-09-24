3894e125cc9a18c454ca0094035a79cc20e95e9e
OmarDevSec <omarsecure30@gmail.com>
2026-09-24 09:30:46 -0500

    Deallocated the VM and changed location to denmarkeast because of regional restrictions.

--- terraform/main.tf
+++ terraform/main.tf
@@ -64,7 +64,7 @@ module "logging" {
 module "compute" {
   source                = "./modules/compute"
   resource_group_name   = azurerm_resource_group.SecLab.name
-  location              = "eastus3"
+  location              = "denmarkeast"
   ssh_public_key        = file ("~/.ssh/azsec-lab-rsa.pub")
   allowed_ssh_source_ip = "104.12.201.55/32" 
 }                                                                                                                                                    
